f20ff18f75f052003d77b101d590fb5654f09e3e
venkateshan-niladri <venkateshan.niladri@freshworks.com>
2020-03-14 10:31:24 +0530

    Initial Commit

---
+++ app/models/employee.rb
@@ -0,0 +1,3 @@
+class Employee < ActiveRecord::Base
+  attr_accessible :dept, :name, :salary
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emp::Application.config.secret_token = '7cd7e6947045f5d7876272426a6b25be2035722cc21ee9d9e59959c2575418d96c111e21ff4fbac8443a15ab6ff1319a7cd7c15d7a82cf05d1f769c2a15a0a6f'
